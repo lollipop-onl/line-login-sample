9b2b044a31e02e32f55d55f54a0eabf358e4b447
Ryoya Tamura <lollipop@simochee.net>
2023-01-28 12:52:24 +0900

    ✨ add line login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import liff from '@line/liff';
+
+await liff.init({
+  liffId: '1657855491-8GAwNwaX',
+});
 
 export const App: React.FC = () => {
   return (
-    <div className='max-w-screen-md mx-auto px-4 py-10'>
-      <article className='prose'>
+    <div className="mx-auto max-w-screen-md px-4 py-10">
+      <article className="prose">
         <h1>react-app</h1>
         <p>This is a React application template.</p>
         <p>with Vite, TypeScript, TailwindCSS and daisyUI</p>
+        <pre>logged in? {liff.isLoggedIn() ? 'yes' : 'no'}</pre>
+        <button className="btn" onClick={() => liff.login()}>
+          Login
+        </button>
       </article>
     </div>
   );
